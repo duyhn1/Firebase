9033ea8bb5d7986a2ef3b85fe1825865be1d28d6
duyhn <duy.hn@glv.co.jp>
2019-01-24 12:58:20 +0900

    commit load old message

--- public/js/firebase/message.js
+++ public/js/firebase/message.js
@@ -2,20 +2,28 @@ class Message {
     constructor(firebase) {
         var isFirstLoad = true;
         var blockNumber = 20;
+        var offset;
+        var isProgress = false;
+        var isOldestMessage = false;
         this.messages = firebase.database().ref('public/messages');
         var currentUser = this.currentUser = firebase.auth().currentUser;
         this.messages.limitToLast(1).on('child_added', function (data) {
             if (!isFirstLoad) {
                 RenderChat(data.toJSON(), true);
+                $('#chat-container').animate( {scrollTop: $('#chat-container').get(0).scrollHeight}, 500 )
             } else {
                 isFirstLoad = false;
             }
         });
         
         this.messages.orderByKey().limitToLast(blockNumber).once('value', function (data) {
+            var firstId;
             data.forEach(e => {
+                if (!firstId) firstId = e.key; 
                 RenderChat(e.val());
             });
+            $('#chat-container').animate( {scrollTop: $('#chat-container').get(0).scrollHeight}, 500 )
+            offset = firstId;
         });
 
         function RenderChat(data, isNotify = false, isPrepend = false) {
@@ -34,7 +42,7 @@ class Message {
             if (isPrepend) {
                 $('#chat-container').prepend(html);
             } else {
-                $('#chat-container').append(html).animate( {scrollTop: $('#chat-container').get(0).scrollHeight}, 500 );
+                $('#chat-container').append(html);
             }
             if (isNotify && data.uid !== currentUser.uid) {
                 Notify(data.user, data.message);
@@ -52,6 +60,26 @@ class Message {
                 };
             }
         }
+        $('#chat-container').scroll(function() {
+            if(!isOldestMessage && !isProgress && $(this).scrollTop() == 0) {
+                isProgress = true;
+                firebase.database().ref('public/messages')
+                .orderByKey().limitToLast(blockNumber + 1).endAt(offset).once('value', function (data) {
+                    if (Object.keys(data.val()).length < blockNumber + 1) {
+                        isOldestMessage = true;
+                    }
+                    var firstId;
+                    data.forEach(e => {
+                        if (e.key !== offset) {
+                            if (!firstId) firstId = e.key; 
+                            RenderChat(e.val(), false, true);
+                        }
+                    });
+                    offset = firstId;
+                    isProgress = false;
+                });
+            }
+        })
     }
     SendChat(msg) {
         this.messages.push({ 
